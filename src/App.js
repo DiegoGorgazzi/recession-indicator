c3c0570421213a08e43dc3e90a1d669df5e18e8a
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-05-21 06:42:42 +0200

    Add container folder structure and RecessionIndicator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import RecessionIndicator from "./containers/RecessionIndicator/RecessionIndicator";
 
 function App() {
   return (
     <div className="App">
 
+      <RecessionIndicator />
 
     </div>
   );
---
+++ src/containers/RecessionIndicator/RecessionIndicator.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import recessionIndicatorStyles from "./RecessionIndicator.module.css";
+
+class RecessionIndicator extends Component {
+
+  render() {
+    return (
+
+      <p>Hello </p>
+
+    )
+
+
+  }
+
+
+}
+
+export default RecessionIndicator;
