bb69f546df9d4378d541df1eff1886e39c8c6695
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-05-24 15:44:25 +0200

    Update API call to filter only data and value

--- src/containers/RecessionIndicator/RecessionIndicator.js
+++ src/containers/RecessionIndicator/RecessionIndicator.js
@@ -19,16 +19,31 @@ class RecessionIndicator extends Component {
   }
 
   componentDidMount () {
+
+    //helper function to filter only Date and Value
+    const filteredResponse = (toBeFiltered) => {
+      const mapFilter = toBeFiltered.map((eachObject) => {
+          const date = eachObject.date;
+          const value = eachObject.value;
+          const filteredObject = {date, value};
+          return filteredObject;
+      });
+    return mapFilter;
+    }
+
     axios.all([
       axios.get(tenYearYield),
       axios.get(threeMonthYield),
       ])
-        .then(axios.spread( (tenYrYldResponse, threeMthYldResponse,
-          nberResponse, willshireResponse, vixResponse ) => {
+        .then(axios.spread( (tenYrYldResponse, threeMthYldResponse ) => {
+          //const filtered10yr = filteredResponse(tenYrYldResponse.data.observations);
+          //const filtered3mo = filteredResponse(threeMthYldResponse.data.observations);
+
             this.setState({
-            tenYearInt: tenYrYldResponse.data.observations,
-            threeMonthInt: threeMthYldResponse.data.observations,
+            tenYearInt: filteredResponse(tenYrYldResponse.data.observations),
+            threeMonthInt: filteredResponse(threeMthYldResponse.data.observations),
             });
+
           console.log(this.state.tenYearInt, "ten");
           console.log(this.state.threeMonthInt, "three");
           }
@@ -36,29 +51,31 @@ class RecessionIndicator extends Component {
 
       axios.get(nberUSrecess).then(nberResponse => {
         this.setState({
-        nberRecession: nberResponse.data.observations
+          nberRecession: filteredResponse(nberResponse.data.observations)
         });
         console.log(this.state.nberRecession, "nber");
       });
 
       axios.get(willshire5000).then(willshireResponse => {
         this.setState({
-        willshireState: willshireResponse.data.observations
+          willshireState: filteredResponse(willshireResponse.data.observations)
         });
         console.log(this.state.willshireState, "willshire");
       });
 
       axios.get(vix).then(vixResponse => {
         this.setState({
-        vixState: vixResponse.data.observations
+          vixState: filteredResponse(vixResponse.data.observations)
         });
         console.log(this.state.vixState, "vix");
       });
 
 }
 
-
   render() {
+
+
+
     return (
     <div>
       <p>Hello </p>
