0c4c95f52ebecfd44c9e8257fb289f73572bb6ee
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-05-27 17:11:41 +0200

    Move mergeResponse function to helperFunction.js

--- src/containers/RecessionIndicator/RecessionIndicator.js
+++ src/containers/RecessionIndicator/RecessionIndicator.js
@@ -10,7 +10,7 @@ import paginationFactory from 'react-bootstrap-table2-paginator';
 import ToolkitProvider, { ColumnToggle } from 'react-bootstrap-table2-toolkit';
 
 //*********************** helperFunctions ******************************
-import {filteredResponse} from "../../shared/helperFunctions/helperFunctions";
+import {filteredResponse, mergedResponse} from "../../shared/helperFunctions/helperFunctions";
 
 //************************ data ****************************************
 import {tenYearYield} from "../../data/fedReserveAPI";
@@ -70,48 +70,6 @@ class RecessionIndicator extends Component {
 }
 
   render() {
-    //helper function to merge two sets of data.
-    //We need to merge data because react-bootstrap-table requires each column
-    //to be part of the same data set. It will also be easier to
-    //graph data if they both have same starting date.
-    const mergedResponse = (mainToBeMerged, additionalArr, name) => {
-      //Since data sets have different starting dates, we need to
-      //check to see which is the longest array to map through
-          if(mainToBeMerged.length > additionalArr.length) {
-            return mainToBeMerged.map((eachObject, index) => {
-              const date = eachObject.date;
-              const value = eachObject.value;
-              const id = name + index;
-              const valueAdd= additionalArr[index].value
-              const mergedObject = {id, date, value, valueAdd};
-              return mergedObject;
-              }
-            );
-          } else {
-              return additionalArr.map((eachObject, index) => {
-                const date = additionalArr[index].date;
-                let value;
-                //the shorter data set needs to start index count only
-                //when the dates of both data sets match.
-                //So while the length of the longer array is longer
-                //than the length of the shorter array, the value will be zero.
-                  if(index < (additionalArr.length - mainToBeMerged.length) ) {
-                    value = 0;
-                  } else {
-                    value =
-                    //since we need to start at mainToBeMerged[0], we need to subtract
-                    //the difference in length in both arrays from the index count.
-                    mainToBeMerged[index-(additionalArr.length - mainToBeMerged.length)]
-                    .value;
-                  }
-                const id = name + index;
-                const valueAdd= eachObject.value;
-                const mergedObject = {id, date, value, valueAdd};
-                return mergedObject;
-              }
-            );
-          }
-        };
 
     const tenThreeMerged = mergedResponse(this.state.tenYearInt, this.state.threeMonthInt, "merged");
     console.log(tenThreeMerged, "merged");
--- src/shared/helperFunctions/helperFunctions.js
+++ src/shared/helperFunctions/helperFunctions.js
@@ -9,3 +9,50 @@ export const filteredResponse = (toBeFiltered, name) => {
       return filteredObject;
   });
 }
+
+
+/******************************************************************/
+//helper function to merge two sets of data.
+//We need to merge data because react-bootstrap-table requires each column
+//to be part of the same data set. It will also be easier to
+//graph data if they both have same starting date.
+export const mergedResponse = (mainToBeMerged, additionalArr, name) => {
+  //Since data sets have different starting dates, we need to
+  //check to see which is the longest array to map through
+      if(mainToBeMerged.length > additionalArr.length) {
+        return mainToBeMerged.map((eachObject, index) => {
+          const date = eachObject.date;
+          const value = eachObject.value;
+          const id = name + index;
+          const valueAdd= additionalArr[index].value
+          const mergedObject = {id, date, value, valueAdd};
+          return mergedObject;
+          }
+        );
+      } else {
+          return additionalArr.map((eachObject, index) => {
+            const date = additionalArr[index].date;
+            let value;
+            //the shorter data set needs to start index count only
+            //when the dates of both data sets match.
+            //So while the length of the longer array is longer
+            //than the length of the shorter array, the value will be zero.
+              if(index < (additionalArr.length - mainToBeMerged.length) ) {
+                value = 0;
+              } else {
+                value =
+                //since we need to start at mainToBeMerged[0], we need to subtract
+                //the difference in length in both arrays from the index count.
+                mainToBeMerged[index-(additionalArr.length - mainToBeMerged.length)]
+                .value;
+              }
+            const id = name + index;
+            const valueAdd= eachObject.value;
+            const mergedObject = {id, date, value, valueAdd};
+            return mergedObject;
+          }
+        );
+      }
+    };
+
+/************************************************************************/
