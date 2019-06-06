09086fd4aef6b313c030ea885ea5d696520e6571
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-06-06 23:07:26 +0200

    Add additional TimeRange elements to TimeRangeController

--- src/components/TimeRangeController/TimeRangeController.js
+++ src/components/TimeRangeController/TimeRangeController.js
@@ -4,9 +4,41 @@ import TimeRange from "../TimeRange/TimeRange";
 const timeRangeController = (props) => (
       <div>
         <TimeRange
+          timeRangeName="2Y"
+          controlClickTimeRange={props.clickTimeRange}
+          timeRangeID="2yrRange" />
+
+        <TimeRange
+          timeRangeName="5Y"
+          controlClickTimeRange={props.clickTimeRange}
+          timeRangeID="5yrRange" />
+
+        <TimeRange
           timeRangeName="10Y"
           controlClickTimeRange={props.clickTimeRange}
           timeRangeID="10yrRange" />
+
+        <TimeRange
+          timeRangeName="20Y"
+          controlClickTimeRange={props.clickTimeRange}
+          timeRangeID="20yrRange" />
+
+        <TimeRange
+          timeRangeName="30Y"
+          controlClickTimeRange={props.clickTimeRange}
+          timeRangeID="30yrRange" />
+
+        <TimeRange
+          timeRangeName="50Y"
+          controlClickTimeRange={props.clickTimeRange}
+          timeRangeID="50yrRange" />
+
+        <TimeRange
+          timeRangeName="All"
+          controlClickTimeRange={props.clickTimeRange}
+          timeRangeID="allyrRange" />
+
+
       </div>
 
     );
