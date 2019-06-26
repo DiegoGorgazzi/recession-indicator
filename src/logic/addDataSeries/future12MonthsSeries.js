7d1f60034e7aef1e5db8270e812f7d0fe39970f9
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-06-26 16:35:43 +0200

    Fix bug when input end date < dates in array

--- src/logic/addDataSeries/future12MonthsSeries.js
+++ src/logic/addDataSeries/future12MonthsSeries.js
@@ -1,13 +1,17 @@
 //FUNCTION to add futures dates (12-months, monthly) to a chart
 export const future12MonthsSeries = (arrayToGetCurrentDate) => {
-  let future12Months = []
+  //I had to copy the array otherwise, when the user 
+  //sets an end date that is smaller than the date available in the array
+  //I get a "date undefined" error, which makes sense.  
+  let future12Months = [...arrayToGetCurrentDate]
   if(arrayToGetCurrentDate.length > 0) {
-    for (let i = 20; i > 0; --i) {
+    for (let i = 12; i > 0; --i) {
       let arrayToGetCurrentDateLast12Date = arrayToGetCurrentDate[arrayToGetCurrentDate.length-i].date
       const futureDateObject = {date: "", value: ""};
       futureDateObject.date = arrayToGetCurrentDateLast12Date;
       future12Months.push(futureDateObject);
     }
   };
+
   return future12Months;
 };
