ab8c6792db476a7e41cd8f01e5897f118afdc9dd
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-07-12 13:54:08 +0200

    Make TimeRangeController and children responsive

--- src/components/TimeRangeController/TimeRangeController.js
+++ src/components/TimeRangeController/TimeRangeController.js
@@ -4,52 +4,60 @@ import TimeUserRange from "../TimeUserRange/TimeUserRange";
 
 const timeRangeController = (props) => (
       <div>
-        <TimeRange
-          timeRangeName="2Y"
-          controlClickTimeRange={props.clickTimeRange}
-          timeRangeID="2yrRange" />
+        <div>
+          <TimeRange
+            timeRangeName="2Y"
+            controlClickTimeRange={props.clickTimeRange}
+            timeRangeID="2yrRange" />
 
-        <TimeRange
-          timeRangeName="5Y"
-          controlClickTimeRange={props.clickTimeRange}
-          timeRangeID="5yrRange" />
+          <TimeRange
+            timeRangeName="5Y"
+            controlClickTimeRange={props.clickTimeRange}
+            timeRangeID="5yrRange" />
 
-        <TimeRange
-          timeRangeName="10Y"
-          controlClickTimeRange={props.clickTimeRange}
-          timeRangeID="10yrRange" />
+          <TimeRange
+            timeRangeName="10Y"
+            controlClickTimeRange={props.clickTimeRange}
+            timeRangeID="10yrRange" />
 
-        <TimeRange
-          timeRangeName="20Y"
-          controlClickTimeRange={props.clickTimeRange}
-          timeRangeID="20yrRange" />
+          <TimeRange
+            timeRangeName="20Y"
+            controlClickTimeRange={props.clickTimeRange}
+            timeRangeID="20yrRange" />
 
-        <TimeRange
-          timeRangeName="30Y"
-          controlClickTimeRange={props.clickTimeRange}
-          timeRangeID="30yrRange" />
+          <TimeRange
+            timeRangeName="30Y"
+            controlClickTimeRange={props.clickTimeRange}
+            timeRangeID="30yrRange" />
 
-        <TimeRange
-          timeRangeName="50Y"
-          controlClickTimeRange={props.clickTimeRange}
-          timeRangeID="50yrRange" />
+          <TimeRange
+            timeRangeName="50Y"
+            controlClickTimeRange={props.clickTimeRange}
+            timeRangeID="50yrRange" />
 
-        <TimeRange
-          timeRangeName="All"
-          controlClickTimeRange={props.clickTimeRange}
-          timeRangeID="allyrRange" />
-        <span> From: </span>
-        <TimeUserRange
-              controlUserTimeState = {props.userStartTimeState}
-              nameDate = "userStartDate"
-              controlUserDateHandler = {props.userDateHandler}
-            />
-        <span> To: </span>
-        <TimeUserRange
-              controlUserTimeState = {props.userEndTimeState}
-              nameDate = "userEndDate"
-              controlUserDateHandler = {props.userDateHandler}
-            />
+          <TimeRange
+            timeRangeName="All"
+            controlClickTimeRange={props.clickTimeRange}
+            timeRangeID="allyrRange" />
+        </div>
+        <div>
+          <div>
+            <span style={{marginRight: "5px"}}> From: </span>
+              <TimeUserRange
+                    controlUserTimeState = {props.userStartTimeState}
+                    nameDate = "userStartDate"
+                    controlUserDateHandler = {props.userDateHandler}
+                  />
+          </div>
+          <div>
+            <span style={{marginRight: "5px"}}> To: </span>
+              <TimeUserRange
+                    controlUserTimeState = {props.userEndTimeState}
+                    nameDate = "userEndDate"
+                    controlUserDateHandler = {props.userDateHandler}
+                  />
+          </div>
+        </div>
         <button onClick = {props.userDateSetter} >
           Apply Date Range
         </button>
