d228745659ddb17890a4514ac2e432e356bae718
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-07-21 09:42:51 +0200

    Remove Comments

--- src/logic/crosshair/otherChartCrosshairVal.js
+++ src/logic/crosshair/otherChartCrosshairVal.js
@@ -2,12 +2,8 @@
 //MUST be used in conjunction with {index} within the onNearestX method
 export const otherChartCrosshairVal = (arrayInThisChart, arrayInOtherChart, indexInThisArray ) => {
     
-    //I NEED TO ADD A COMPARISON... ?? IF ARRAYinotherchart.length > arrayInThis chart, then 
     return ({
     x: arrayInOtherChart[arrayInOtherChart.length-(arrayInThisChart.length-indexInThisArray)] !== undefined ? arrayInOtherChart[arrayInOtherChart.length-(arrayInThisChart.length-indexInThisArray)].x: null,
     y: arrayInOtherChart[arrayInOtherChart.length-(arrayInThisChart.length-indexInThisArray)] !== undefined ? arrayInOtherChart[arrayInOtherChart.length-(arrayInThisChart.length-indexInThisArray)].y: null
     })
 }
-
-//WAIT... I THINK THIS IS FINE, 
-//I think THE ORDER IN RecessionIndicator.js is wrong
