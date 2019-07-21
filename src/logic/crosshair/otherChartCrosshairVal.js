c56a359f49112f9ee8e22d91ed8b77ab1845fa2b
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-07-21 09:41:48 +0200

    Fix crosshair for other charts when date is "All"

--- src/logic/crosshair/otherChartCrosshairVal.js
+++ src/logic/crosshair/otherChartCrosshairVal.js
@@ -1,8 +1,13 @@
 //Function used to get the value (x and y) when the mouse pointer is in a different chart
 //MUST be used in conjunction with {index} within the onNearestX method
 export const otherChartCrosshairVal = (arrayInThisChart, arrayInOtherChart, indexInThisArray ) => {
+    
+    //I NEED TO ADD A COMPARISON... ?? IF ARRAYinotherchart.length > arrayInThis chart, then 
     return ({
     x: arrayInOtherChart[arrayInOtherChart.length-(arrayInThisChart.length-indexInThisArray)] !== undefined ? arrayInOtherChart[arrayInOtherChart.length-(arrayInThisChart.length-indexInThisArray)].x: null,
     y: arrayInOtherChart[arrayInOtherChart.length-(arrayInThisChart.length-indexInThisArray)] !== undefined ? arrayInOtherChart[arrayInOtherChart.length-(arrayInThisChart.length-indexInThisArray)].y: null
     })
 }
+
+//WAIT... I THINK THIS IS FINE, 
+//I think THE ORDER IN RecessionIndicator.js is wrong
